01cc649bedfcd43532e5bb283dbb0f548d87c914
VitorSouzaC <147544013+VitorSouzaC@users.noreply.github.com>
2025-08-13 16:32:39 -0300

    par-ou-impar.c

--- par-ou-impar.c
+++ par-ou-impar.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main(){
+  int num;
+  printf("Digite um numero: ");
+  scanf("%d", &num);
+  if(num % 2 == 0){
+printf("1");
+  } else{
+    printf("0");
+      }
 
+  return 0;
+}
