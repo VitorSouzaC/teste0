b0f04ec43df472297717df12ae2203ee39344d88
VitorSouzaC <147544013+VitorSouzaC@users.noreply.github.com>
2025-08-13 16:35:23 -0300

    Melhorado par-ou-impar.c

--- par-ou-impar.c
+++ par-ou-impar.c
@@ -3,11 +3,6 @@ int main(){
   int num;
   printf("Digite um numero: ");
   scanf("%d", &num);
-  if(num % 2 == 0){
-printf("1");
-  } else{
-    printf("0");
-      }
-
+  printf("%d", !(n % 2));
   return 0;
 }
